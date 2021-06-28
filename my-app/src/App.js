3e321d6503910da05c1615fa90d57849fc78dee9
Eileen <lai.eling22@gmail.com>
2021-06-28 23:31:45 +0800

    ✨ implement routing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,6 +9,12 @@ import "firebase/firestore";
 // import admin from "firebase-admin";
 // import serviceAccount from "stock-collect-firebase-adminsdk.json";
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 // const d3 = require("d3");
 
@@ -113,8 +119,8 @@ const FaceCircle = (props) => (
   <circle r={props.r} fill={props.color}/>
 );
 
-function App() {
-  return (
+function Home() {
+  return(
     <div className="App">
       <header className="App-header">
         <svg width="960" height="500">
@@ -126,6 +132,50 @@ function App() {
         </p>
       </header>
     </div>
+  );
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+function App() {
+  return (
+    <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+  
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/users">
+              <Users />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
   );
 }
 
